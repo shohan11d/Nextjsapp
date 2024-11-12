a5bc5c674ae48a24f95dfeb98e97efed7f1425f6
shohan11d <shohan11d@gmail.com>
2024-11-12 18:06:22 +0600

    navigation not loaded property when clicking fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,18 +3,30 @@ import { Link } from 'react-router-dom';
 function Menu() {
   return (
     <div className="flex list-none gap-5 p-4">
-      <li className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400 transition-all">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400">
-        <Link to="/pizzamenu">pizzamenu</Link>
-      </li>
-      <li className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400">
-        <Link to="/order">order</Link>
-      </li>
-      <li className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400">
-        <Link to="/cart">cart</Link>
-      </li>
+      <Link
+        className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400"
+        to="/home"
+      >
+        Home
+      </Link>
+      <Link
+        className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400"
+        to="/order"
+      >
+        Order
+      </Link>
+      <Link
+        className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400"
+        to="/pizzamenu"
+      >
+        PizzaMenu
+      </Link>
+      <Link
+        className="rounded-full bg-green-600 px-4  py-1 hover:bg-yellow-400"
+        to="/cart"
+      >
+        Cart
+      </Link>
     </div>
   );
 }
